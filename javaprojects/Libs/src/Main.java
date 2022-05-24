455c34e42e38051ef17a11d80030e4e5b3bd1202
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-05-25 00:10:10 +0530

    initial checkin

---
+++ javaprojects/Libs/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String gridCustomization = "{\"pageSize\":25}";
+        System.out.println(gridCustomization);
+    }
+}
